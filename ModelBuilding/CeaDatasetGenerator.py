6fe81ff98fc9009c807d0a6b5e4b5ec063f38982
ecfedele <118453623+ecfedele@users.noreply.github.com>
2023-01-11 18:41:46 -0500

    Rebuilt CeaDatasetGenerator.py
    Rebuilt CeaDatasetGenerator.py. In the first version, the input data range was pregenerated; this simplified encapsulation of the functions but led to an issue whereby some data might lie outside of the scope of CEA to generate results. The newer version corrects for this by testing (using regular expressions) for CEA validity to ensure the viability of the output data.

--- ModelBuilding/CeaDatasetGenerator.py
+++ ModelBuilding/CeaDatasetGenerator.py
@@ -27,131 +27,148 @@ import numpy             as     np
 import pandas            as     pd
 from   rocketcea.cea_obj import CEA_Obj
 
-## @class CeaDatasetGenerator
-#  Encapsulates logic which generates an exemplary training dataset using Monte Carlo methods and a statistically
-#  uniform source for input generation to CEA.
 class CeaDatasetGenerator:
     fuel = ""
     oxidizer = ""
     elements = 0
 
     ## Constructor for a CeaDatasetGenerator object. Stores user selections for future invocation (i.e. when 
-    #  self.get_cea_data() is called) and prepares the input domain.
+    #  self.get_cea_data() is called) and prepares data domain information for use in Monte Carlo generation.
     #
     #  @param self      The reference to the calling CeaDatasetGenerator object
     #  @param fuel      A string name of the chemical used as fuel in combustion. Passed directly to CEA.
     #  @param oxidizer  A string name of the chemical used as oxidizer in combustion. Passed directly to CEA.
-    #  @param n         The number of elements to populate the input domain sample with.
+    #  @param p_min     An optional parameter which specifies the lower limit of pressure (in bar, 10^5 Pa)
+    #  @param p_max     An optional parameter which specifies the upper limit of pressure (in bar, 10^5 Pa)
+    #  @param phi_min   An optional parameter which specifies the lower limit of propellant mixture ratio
+    #  @param phi_max   An optional parameter which specifies the upper limit of propellant mixture ratio
+    #  @param eps_min   An optional parameter which specifies the lower limit of nozzle expansion area ratio
+    #  @param eps_max   An optional parameter which specifies the upper limit of nozzle expansion area ratio
+    #  @param n         An optional parameter indicating the desired sample size.
     #
     #  @returns         None (constructor)
-    def __init__(self, fuel, oxidizer, n=10000):
+    def __init__(self, fuel, oxidizer, p_min=2.5, p_max=750.0, phi_min=0.01, phi_max=50.0, eps_min=1.0, 
+                 eps_max=200.0, n=10000):
         self.fuel       = fuel
         self.oxidizer   = oxidizer
         self.elements   = n
+        self.p_range    = [ p_min, p_max ]
+        self.phi_range  = [ phi_min, phi_max ]
+        self.eps_range  = [ eps_min, eps_max ]
         self.cea        = CEA_Obj(oxName=self.oxidizer, fuelName=self.fuel)
-        (self.pressure, self.mixture, self.area_ratio) = self.__get_domains__()
-        self.input_data = self.get_input_dataframe()
+        col_names       = [ "fuel", "oxidizer", "pressure", "mixture", "area_ratio", "pressure_throat", 
+                            "pressure_exit", "molar_mass_chamber", "molar_mass_throat", "molar_mass_exit", 
+                            "adiabat_chamber", "adiabat_throat", "adiabat_exit", "temperature_chamber", 
+                            "temperature_throat", "temperature_exit", "rho_chamber", "rho_throat", "rho_exit", 
+                            "spec_heat_chamber", "spec_heat_throat", "spec_heat_exit", "visc_chamber", 
+                            "visc_throat", "visc_exit", "cond_chamber", "cond_throat", "cond_exit", 
+                            "prandtl_chamber", "prandtl_throat", "prandtl_exit", "mach_exit" ]
+        self.data       = pd.DataFrame(columns=col_names)
 
-    ## Obtain CEA data for entire listed input domain (i.e. the dataframe contained in self.input_data).
+    ## Fills out the self.data DataFrame to a size of self.elements with CEA data
     #
     #  @param self      The reference to the calling CeaDatasetGenerator object
     #
-    #  @returns         A Pandas DataFrame consisting of the input domain plus CEA results
+    #  @returns         The DataFrame constructed (self.data)
     def get_cea_data(self):
-        pressure_throat     = []
-        pressure_exit       = []
-        molar_mass_chamber  = []
-        molar_mass_throat   = []
-        molar_mass_exit     = []
-        adiabat_chamber     = []
-        adiabat_throat      = []
-        adiabat_exit        = []
-        temperature_chamber = []
-        temperature_throat  = []
-        temperature_exit    = []
-        rho_chamber         = []
-        rho_throat          = []
-        rho_exit            = []
-        spec_heat_chamber   = []
-        spec_heat_throat    = []
-        spec_heat_exit      = []
-        visc_chamber        = []
-        visc_throat         = []
-        visc_exit           = []
-        cond_chamber        = []
-        cond_throat         = []
-        cond_exit           = []
-        prandtl_chamber     = []
-        prandtl_throat      = []
-        prandtl_exit        = []
-        
-        for index, row in self.input_data.iterrows():
-            cea_output = self.cea.get_full_cea_output(Pc=row['pressure'], 
-                                                      MR=row['mixture'], 
-                                                      eps=row['area_ratio'], 
+        while len(self.data.index) < self.elements:
+            pressure   = np.random.default_rng().uniform(low=self.p_range[0],   high=self.p_range[1])
+            mixture    = np.random.default_rng().uniform(low=self.phi_range[0], high=self.phi_range[1])
+            area_ratio = np.random.default_rng().uniform(low=self.eps_range[0], high=self.eps_range[1])
+            cea_fostr  = self.cea.get_full_cea_output(Pc=pressure, 
+                                                      MR=mixture, 
+                                                      eps=area_ratio, 
                                                       short_output=1, 
-                                                      show_transport=1, 
+                                                      show_transport=1,
+                                                      output='siunits',
                                                       pc_units='bar')
-            extraction_dict = self.__extract_properties__(cea_output)
-            
+            if self.is_valid_cea_result(cea_fostr):
+                (pressure_throat, pressure_exit) = self.get_pressures(cea_fostr)
+                (molar_mass_chamber, molar_mass_throat, molar_mass_exit) = self.get_molar_masses(cea_fostr)
+                (adiabat_chamber, adiabat_throat, adiabat_exit) = self.get_adiabat(cea_fostr)
+                (temperature_chamber, temperature_throat, temperature_exit) = self.get_temperatures(cea_fostr)
+                (spec_heat_chamber, visc_chamber, cond_chamber, prandtl_chamber) = self.cea.get_Chamber_Transport(
+                                                                                       Pc=(pressure * 14.5038),
+                                                                                       MR=mixture,
+                                                                                       eps=area_ratio 
+                                                                                   )
+                (spec_heat_throat, visc_throat, cond_throat, prandtl_throat) = self.cea.get_Throat_Transport(
+                                                                                   Pc=(pressure * 14.5038),
+                                                                                   MR=mixture,
+                                                                                   eps=area_ratio 
+                                                                               )
+                (spec_heat_exit, visc_exit, cond_exit, prandtl_exit) = self.cea.get_Exit_Transport(
+                                                                           Pc=(pressure * 14.5038),
+                                                                           MR=mixture,
+                                                                           eps=area_ratio 
+                                                                       )
+                self.data.loc[len(self.data.index)] = [ self.fuel, self.oxidizer, pressure, mixture, area_ratio,
+                                                        pressure_throat, pressure_exit, molar_mass_chamber, 
+                                                        molar_mass_throat, molar_mass_exit, adiabat_chamber, 
+                                                        adiabat_throat, adiabat_exit, temperature_chamber, 
+                                                        temperature_throat, temperature_exit ]
 
-    ## Returns a Pandas DataFrame object consisting of the input domain selections.
+    ##
     #
-    #  @param self      The reference to the calling CeaDatasetGenerator object
     #
-    #  @returns         a DataFrame consisting of the input domain
-    def get_input_dataframe(self):
-        d = {'pressure': self.pressure, 'mixture': self.mixture, 'area_ratio': self.area_ratio}
-        return pd.DataFrame(data=d)
+    #
+    #
+    #
+    def is_valid_cea_result(self, cea_fostr):
+        exit_regexp = re.search(r"EXIT", cea_fostr)
+        nan_regexp  = re.search(r"NaN",  cea_fostr)
+        if (exit_regexp is not None) and (nan_regexp is None):
+            return True
+        else:
+            return False
 
-    ## Generates the three principle Monte Carlo input lists based on minimum/maximum selections by the caller.
+    ##
     #
-    #  @param self      The reference to the calling CeaDatasetGenerator object
-    #  @param p_min     The minimum pressure (rated in bar, 10^5 Pa) to consider within the dataset
-    #  @param p_max     The maximum pressure (rated in bar, 10^5 Pa) to consider within the dataset 
-    #  @param phi_min   The minimum mixture ratio (u/l) to consider within the dataset
-    #  @param phi_max   The maximum mixture ratio (u/l) to consider within the dataset
-    #  @param eps_min   The minimum area ratio (u/l) to consider within the dataset
-    #  @param n         The size of the dataset to generate
     #
-    #  @returns         a tuple of Numpy arrays consisting of the dataset dimensions
-    def __get_domains__(self, p_min=2.5, p_max=750.0, phi_min=0.01, phi_max=50.0, eps_min=1.0, eps_max=200.0):
-        pc  = np.random.default_rng().uniform(low=p_min, high=p_max, size=self.elements)
-        phi = np.random.default_rng().uniform(low=phi_min, high=phi_max, size=self.elements)
-        eps = np.random.default_rng().uniform(low=eps_min, high=eps_max, size=self.elements)
-        return (pc, phi, eps)
+    #
+    #
+    #
+    def get_pressures(self, cea_fostr):
+        pressure_results = re.search(r"P, ATM\s*\d+.\d+\s*(\d+.\d+)\s*(\d+.\d+)", cea_fostr)
+        pressure_throat  = float(pressure_results(1)) * 1.01325
+        pressure_exit    = float(pressure_results(2)) * 1.01325
+        return (pressure_throat, pressure_exit)
 
-    ## Pseudo-private method which uses regular expressions to extract relevant thermodynamic parameters from CEA 
-    #  full output string.
-    #  
-    #  @param self       The reference to the calling CeaDatasetGenerator object
-    #  @param cea_string The full output string received from CEA_Obj.get_full_cea_output()
+    ##
     #
-    #  @returns          A dictionary consisting of relevant parameters for use by self.get_cea_data()
-    def __extract_properties__(self, cea_string):
-        pressure_results    = re.search(r"P, ATM\s*\d+.\d+\s*(\d+.\d+)\s*(\d+.\d+)", cea_string)
+    #
+    #
+    #
+    #
+    def get_molar_masses(self, cea_fostr):
+        molar_mass_results = re.search(r"M, \(1/n\)\s*(\d+.\d+)\s*(\d+.\d+)\s*(\d+.\d+)", cea_fostr)
+        molar_mass_chamber = float(molar_mass_results(1))
+        molar_mass_throat  = float(molar_mass_results(2))
+        molar_mass_exit    = float(molar_mass_results(3))
+        return (molar_mass_chamber, molar_mass_throat, molar_mass_exit)
+
+    ##
+    #
+    #
+    #
+    #
+    #
+    def get_adiabat(self, cea_fostr):
+        adiabat_results = re.search(r"GAMMAs\s*(\d+.\d+)\s*(\d+.\d+)\s*(\d+.\d+)", cea_fostr)
+        adiabat_chamber = float(adiabat_results(1))
+        adiabat_throat  = float(adiabat_results(2))
+        adiabat_exit    = float(adiabat_results(3))
+        return (adiabat_chamber, adiabat_throat, adiabat_exit)
+
+    ##
+    #
+    #
+    #
+    #
+    #
+    def get_temperatures(self, cea_fostr):
         temperature_results = re.search(r"T, K\s*(\d+.\d+)\s*(\d+.\d+)\s*(\d+.\d+)", cea_string)
-        molar_mass_results  = re.search(r"M, \(1/n\)\s*(\d+.\d+)\s*(\d+.\d+)\s*(\d+.\d+)", cea_string)
-        adiabat_results     = re.search(r"GAMMAs\s*(\d+.\d+)\s*(\d+.\d+)\s*(\d+.\d+)", cea_string)
-        pressure_throat     = float(pressure_results(1)) * 1.01325
-        pressure_exit       = float(pressure_results(2)) * 1.01325
-        molar_mass_chamber  = float(molar_mass_results(1))
-        molar_mass_throat   = float(molar_mass_results(2))
-        molar_mass_exit     = float(molar_mass_results(3))
-        adiabat_chamber     = float(adiabat_results(1))
-        adiabat_throat      = float(adiabat_results(2))
-        adiabat_exit        = float(adiabat_results(3))
         temperature_chamber = float(temperature_results(1))
         temperature_throat  = float(temperature_results(2))
         temperature_exit    = float(temperature_results(3))
-        return { "pressure_throat":     pressure_throat, 
-                 "pressure_exit":       pressure_exit,
-                 "molar_mass_chamber":  molar_mass_chamber,
-                 "molar_mass_throat":   molar_mass_throat,
-                 "molar_mass_exit":     molar_mass_exit,
-                 "adiabat_chamber":     adiabat_chamber,
-                 "adiabat_throat":      adiabat_throat,
-                 "adiabat_exit":        adiabat_exit,
-                 "temperature_chamber": temperature_chamber,
-                 "temperature_throat":  temperature_throat,
-                 "temperature_exit":    temperature_exit }
+        return (temperature_chamber, temperature_throat, temperature_exit)
